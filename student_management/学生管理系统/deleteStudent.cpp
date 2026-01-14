5bab83aa23b68bc5dd4a014db368460ba05e6d8a
lose-sea <2031938936@qq.com>
2026-01-14 11:57:32 +0800

    test

--- student_management/学生管理系统/deleteStudent.cpp
+++ student_management/学生管理系统/deleteStudent.cpp
@@ -9,6 +9,33 @@ void deleteSrudent(struct student* stu) {
 		system("pause"); 
 		return; 
 	} 
-	printf("请输入你要删除的学生姓名"); 
-	while ()
+	printf("请输入你要删除的学生姓名: "); 
+	char name[300];
+	fgets(name, 300, stdin);
+	removeNewline(name);
+	struct ListNode* curr = stu->head;
+	while (curr != NULL) { 
+		if (strcmp(name, curr->name) == 0) {
+			break; 
+		}
+		curr = curr->next;
+	}
+	if (curr == NULL) {
+		printf("不存在学生 %s 的信息\n", name);
+		system("pause");
+		return;
+	} else {
+		// 找到学生信息，进行删除
+		struct ListNode* prev = stu->head;
+		while (prev->next != curr) {
+			prev = prev->next;
+		}
+		prev->next = curr->next; 
+		curr->next->prev = prev; 
+		free(curr); 
+		stu->studentSize--;		// 删除成功，学生数量减一
+		printf("已成功删除学生 %s 的信息\n", name);
+		system("pause");
+		return;
+	}
 }
