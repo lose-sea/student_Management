244801b8a3b94a5de372fc945a3a73829bcc8fcf
lose_sea <2031938936@qq.com>
2026-01-20 11:10:36 +0800

    test

--- student_management/学生管理系统/main.cpp
+++ student_management/学生管理系统/main.cpp
@@ -5,10 +5,17 @@ int main() {
 	stu.head = NULL;  
 	stu.tail = NULL;
 	stu.studentSize = 0; 
-	loadFromFile(&stu); 
-	displayAllStudent(&stu);
-	// addStudent(&stu);  
-	deleteStudent(&stu); 
+	loadFromFile(&stu);  
+	struct ListNode* curr = stu.head;  
+	/*while (curr->next != NULL) {
+		curr = curr->next;
+	} 
+	free(curr); 
+	stu.studentSize--;*/
+
+	displayAllStudent(&stu); 
+	addStudent(&stu);  
+	//deleteStudent(&stu); 
 	saveToFile(&stu);
 	return 0;
 }
